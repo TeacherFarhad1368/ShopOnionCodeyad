3f516fdd0d4329f09259edb836395f156676561b
TeacherFarhad1368 <TeacherFarhad.Net@gmail.com>
2024-03-24 18:36:01 +0330

    97

---
+++ ShopBoloor.WebApplication/Areas/Admin/Controllers/Post/PostController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostModule.Application.Contract.PostApplication;
+using PostModule.Application.Contract.PostQuery;
+using System.Diagnostics.Contracts;
+
+namespace ShopBoloor.WebApplication.Areas.Admin.Controllers.Post
+{
+    [Area("Admin")]
+    public class PostController : Controller
+    {
+        private readonly IPostQuery _postQuery;
+        private readonly IPostApplication _postApplication;
+
+        public PostController(IPostQuery postQuery, IPostApplication postApplication)
+        {
+            _postQuery = postQuery;
+            _postApplication = postApplication;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_postQuery.GetAllPostsForAdmin());
+        }
+        public IActionResult Create() => View();
+        [HttpPost] 
+        public IActionResult Create(CreatePost model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var res = _postApplication.Create(model);
+            if(res.Success)
+            {
+                TempData["ok"] = true;
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError(res.ModelName, res.Message);
+            return View(model);
+        }
+        public IActionResult Edit(int id) => View(_postApplication.GetForEdit(id));
+        [HttpPost]
+        public IActionResult Edit(int id,EditPost model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var res = _postApplication.Edit(model);
+            if (res.Success)
+            {
+                TempData["ok"] = true;
+                return Redirect($"/Admin/Post/Edit/{id}");
+            }
+            ModelState.AddModelError(res.ModelName, res.Message);
+            return View(model);
+        }
+        public bool Active(int id) => _postApplication.ActivationChange(id);
+        public bool Inside(int id) => _postApplication.InsideCityChange(id);
+        public bool Outside(int id) => _postApplication.OutSideCityChange(id);
+    }
+}
---
+++ ShopBoloor.WebApplication/Areas/Admin/Controllers/Post/PostPriceController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostModule.Application.Contract.PostApplication;
+using PostModule.Application.Contract.PostPriceApplication;
+using PostModule.Application.Contract.PostQuery;
+
+namespace ShopBoloor.WebApplication.Areas.Admin.Controllers.Post
+{
+    [Area("Admin")]
+    public class PostPriceController : Controller
+    {
+        private readonly IPostQuery _postQuery;
+        private readonly IPostPriceApplication _postPriceApplication;
+
+        public PostPriceController(IPostQuery postQuery, IPostPriceApplication postPriceApplication)
+        {
+            _postQuery = postQuery;
+            _postPriceApplication = postPriceApplication;
+        }
+
+        public IActionResult Index(int id)
+        {
+            return View(_postQuery.GetPostDetails(id));
+        }
+        public IActionResult Create(int id) => View(new CreatePostPrice { PostId = id });
+        [HttpPost]
+        public IActionResult Create(int id,CreatePostPrice model)
+        {
+            if (id != model.PostId) return NotFound();
+            if (!ModelState.IsValid) return View(model);
+            var res = _postPriceApplication.Create(model);
+            if (res.Success)
+            {
+                TempData["ok"] = true;
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError(res.ModelName, res.Message);
+            return View(model);
+        }
+        public IActionResult Edit(int id) => View(_postPriceApplication.GetForEdit(id));
+        [HttpPost]
+        public IActionResult Edit(int id,EditPostPrice model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var res = _postPriceApplication.Edit(model);
+            if (res.Success)
+            {
+                TempData["ok"] = true;
+                return Redirect($"/Admin/PostPrice/Edit/{id}");
+            }
+            ModelState.AddModelError(res.ModelName, res.Message);
+            return View(model);
+        }
+    }
+}
