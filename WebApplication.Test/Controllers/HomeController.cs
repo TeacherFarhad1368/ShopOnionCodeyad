45ea375d155e6074af9dffbd83b422b85fd89c15
TeacherFarhad1368 <TeacherFarhad.Net@gmail.com>
2024-05-10 19:24:22 +0330

    - 158

--- WebApplication.Test/Controllers/HomeController.cs
+++ WebApplication.Test/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net;
 using WebApplication.Test.Models;
 
 namespace WebApplication.Test.Controllers
@@ -15,7 +17,34 @@ namespace WebApplication.Test.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<State> model = new();
+            try
+            {
+                // Define the API endpoint URL
+                string apiUrl = "https://localhost:44324/api/shipping";
+
+                // Create a WebRequest
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+                request.Method = "GET";
+
+                // Get the response
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    
+                        // Read the response content
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            string responseBody = reader.ReadToEnd();
+                        model = JsonConvert.DeserializeObject<List<State>>(responseBody);
+                        }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ WebApplication.Test/Models/City.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication.Test.Models
+{
+    public class City
+    {
+        public int CityCode { get; set; }
+        public string Name { get; set; }
+    }
+    public class State
+    {
+        public string Name { get; set; }
+        public List<City> Cities { get; set; }
+    }
+}
