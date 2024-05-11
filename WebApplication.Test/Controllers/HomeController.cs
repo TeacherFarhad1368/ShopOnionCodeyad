ae741a20ebea971501a9db28f4f1bc3fd09853cf
TeacherFarhad1368 <TeacherFarhad.Net@gmail.com>
2024-05-11 16:34:52 +0330

    159

--- WebApplication.Test/Controllers/HomeController.cs
+++ WebApplication.Test/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net;
+using System.Text;
 using WebApplication.Test.Models;
 
 namespace WebApplication.Test.Controllers
@@ -46,7 +47,58 @@ namespace WebApplication.Test.Controllers
             }
             return View(model);
         }
+        [HttpPost]
+        public JsonResult GetPrice(int source,int destination,int weight)
+        {
+            PostPriceResponseApiModel res = new PostPriceResponseApiModel();
+            try
+            {
+                // Define the API endpoint URL
+                string apiUrl = "https://localhost:44324/api/shipping";
 
+                // Create an HttpWebRequest for POST
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+                request.Method = "POST";
+                request.ContentType = "application/json"; // Set content type to JSON
+
+                // Create your JSON data (replace with your actual data)
+                string jsonData = "{\"ApiCode\": \"6b4f1695-cdd4-46cc-8d45-36ce46c76e18\"," +
+                    " \"DestinationCityId\":" + $" \"{destination}\"" +
+                    ", \"SourceCityId\":"+$" \"{source}\", \"Weight\": "+$" \"{weight}\""+"}";
+
+                // Convert JSON string to bytes
+                byte[] dataBytes = Encoding.UTF8.GetBytes(jsonData);
+
+                // Write data to the request stream
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(dataBytes, 0, dataBytes.Length);
+                }
+
+                // Get the response
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        // Read the response content
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            string responseBody = reader.ReadToEnd();
+                             res = JsonConvert.DeserializeObject<PostPriceResponseApiModel>(responseBody);
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json(JsonConvert.SerializeObject(res));
+        }
         public IActionResult Privacy()
         {
             return View();
--- WebApplication.Test/Models/City.cs
+++ WebApplication.Test/Models/City.cs
@@ -10,4 +10,25 @@
         public string Name { get; set; }
         public List<City> Cities { get; set; }
     }
+    public class PostPriceRequestModel
+    {
+        public string ApiCode { get; set; }
+        public int SourceCityId { get; set; }
+        public int DestinationCityId { get; set; }
+        public int Weight { get; set; }
+    }
+    public class PostPriceResponseModel
+    {  
+
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public int Price { get; set; }
+    }
+    public class PostPriceResponseApiModel
+    {
+
+        public List<PostPriceResponseModel> Prices { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
 }
