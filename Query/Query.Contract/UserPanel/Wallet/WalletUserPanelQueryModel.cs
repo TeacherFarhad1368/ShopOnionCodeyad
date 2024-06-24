0c4e2d32645d3ed71dd32e6c1a0fb41bde3a6c2a
TeacherFarhad1368 <TeacherFarhad.Net@gmail.com>
2024-06-24 05:32:58 +0330

    164_Add_Wallet_To_UserPanel

--- Query/Query.Contract/UserPanel/Wallet/WalletUserPanelQueryModel.cs
+++ Query/Query.Contract/UserPanel/Wallet/WalletUserPanelQueryModel.cs
@@ -5,7 +5,6 @@ namespace Query.Contract.UserPanel.Wallet;
 public class WalletUserPanelQueryModel
 {
     public int Id { get; set; }
-    public int OwnerId { get; set; }
     public int Price { get; set; }
     public WalletType Type { get; set; }
     public string Description { get; set; }
---
+++ ShopBoloor.WebApplication/Areas/UserPanel/Controllers/WalletController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Query.Contract.UserPanel.Wallet;
+using Shared.Application.Services.Auth;
+
+namespace ShopBoloor.WebApplication.Areas.UserPanel.Controllers
+{
+    [Authorize]
+    [Area("UserPanel")]
+    public class WalletController : Controller
+    {
+        private int _userId;
+        private readonly IAuthService _authService;
+        private readonly IUserPanelWalletQuery _userPanelWalletQuery;
+        public WalletController(IAuthService authService,IUserPanelWalletQuery userPanelWalletQuery)
+        {
+            _authService = authService;
+            _userPanelWalletQuery = userPanelWalletQuery;   
+        }
+        public IActionResult Index(int pageId,string filter)
+        {
+            _userId = _authService.GetLoginUserId();
+            var model = _userPanelWalletQuery.GetWalletsForUserPanel(_userId,pageId,filter);    
+            return View(model);
+        }
+        public IActionResult Transactions(int pageId, string filter)
+        {
+            _userId = _authService.GetLoginUserId();
+            var model = _userPanelWalletQuery.GetTransactionsForUserPanel(_userId, pageId, filter);
+            return View(model);
+        }
+
+    }
+}
